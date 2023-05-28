36a8a5068466620fea4f37f1af21ab31f0f6775d
arioum <amoghrao121@gmail.com>
2023-05-28 13:42:55 +0530

    first commit

---
+++ component/navbar.tsx
@@ -0,0 +1,16 @@
+const navbar = () => {
+  return (
+    <nav>
+      <div className="logo"></div>
+      <div className="link-container">
+        <ul>
+          <li>About</li>
+          <li>Pricing</li>
+          <li>Help</li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default navbar
